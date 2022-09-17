1798207c46f5aa961dfe73b6dfd01784216bcc04
tkjelds <tkjelds385@gmail.com>
2022-09-17 18:19:02 +0200

    added a massive amount of wizards

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,9 +1,4 @@
-﻿var student = new ImmutableStudent(1,"Tore","Kjelds",DateTime.ParseExact("08/10/2018","dd/MM/yyyy", null),
-        DateTime.ParseExact("10/10/2023","dd/MM/yyyy", null),DateTime.ParseExact("10/10/2023","dd/MM/yyyy", null));
-        
-
-Console.WriteLine(student);
-foreach (var wizard in WizardCollection.Create())
+﻿foreach (var wizard in WizardCollection.Create())
 {
     Console.WriteLine(wizard);
 }
